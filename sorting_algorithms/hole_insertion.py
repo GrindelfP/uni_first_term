7bb60acb8fa01898f399c11bb213bc417cc52763
Grindelf <63808275+GrindelfP@users.noreply.github.com>
2021-12-29 00:23:58 +0300

    Update hole_insertion.py

--- sorting_algorithms/hole_insertion.py
+++ sorting_algorithms/hole_insertion.py
@@ -1,6 +1,6 @@
 def hole_insertion_sort(input_list: list) -> list:
     for i in range(0, len(input_list)):
-        tmp: list = input_list[i]
+        tmp: int = input_list[i]
         hole: int = i
         while hole > 0 and input_list[hole - 1] > tmp:
             hole -= 1
