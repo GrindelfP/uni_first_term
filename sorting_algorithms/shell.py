e39f8f42e4f0d2573843c20bc555821e44ef3d12
Grindelf <63808275+GrindelfP@users.noreply.github.com>
2021-12-29 00:24:30 +0300

    Update shell.py

--- sorting_algorithms/shell.py
+++ sorting_algorithms/shell.py
@@ -2,7 +2,7 @@ def shell_sorting(input_list: list) -> list:
     step: int = len(input_list) // 2
     while step > 0:
         for i in range(step, len(input_list)):
-            tmp: list = input_list[i]
+            tmp: int = input_list[i]
             hole: int = i
             while hole > step - 1 and input_list[hole - step] > tmp:
                 hole -= step
